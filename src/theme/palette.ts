e3d7bc6d659b1128b368087a039006a56c80d072
ivie-gif <emwiongbon_ivie@yahoo.com>
2023-09-08 18:07:50 +0100

    color palettes

---
+++ src/theme/palette.ts
@@ -0,0 +1,20 @@
+const palette = {
+// red color
+primary: {
+    main: 'ff014f'
+},
+// black color
+background: {
+    main: '#212529'
+},
+// blur white color
+secondary: {
+    main: '#c4cfde'
+},
+// heading color
+text: {
+    main: '#ffffff'
+}
+}
+
+export default palette;
