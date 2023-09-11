fc2cacfe1cc87324ff3da0a4d55a9ce2a51ed413
ivie-gif <emwiongbon_ivie@yahoo.com>
2023-09-11 17:15:13 +0100

    header section and responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Header from "./components/header";
 import Navbar from "./components/navbar";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <div>
         <Navbar />
+        <Header />
       </div>
     </div>
   );
