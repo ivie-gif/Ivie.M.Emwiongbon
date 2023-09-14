5ced78ed836e79b14dfa88c6c9fef3e61b27d4e2
ivie-gif <emwiongbon_ivie@yahoo.com>
2023-09-14 15:03:38 +0100

    initializing skills data

---
+++ src/db/skillsData.ts
@@ -0,0 +1,7 @@
+export const frontEnd = ['HTML/HTML5', 'React', 'Typescript', 'Next.Js', 'React Query', 'Redux']
+
+export const backEnd = ['Express.Js', 'Node.Js']
+
+export const cssFrameworks = ['CSS/CSS3', 'MaterialUI', 'TailwindCSS', 'BootStrap', 'StoryBook', 'Styled Component', 'Reactstrap']
+
+export const dataBase = ['MongoDB', 'Firebase']
